2e5677afca0809960703fbdbc2ff8955fb92814b
Axel Sorenson <AxelPSorenson@gmail.com>
2024-08-14 15:04:28 -0700

    Effective Haskell Chapter 2 progress

---
+++ effective-haskell/chapter2/FoldExamples.hs
@@ -0,0 +1,40 @@
+module FoldExamples where
+import Prelude hiding (foldl, foldr)
+
+-- This function has the "shape" of any general recursive function over a list.
+-- isBalanced s = 
+--     0 == isBalanced' 0 s
+--     where
+--        isBalanced' count s
+--         | null s = count
+--         | head s == '(' = isBalanced' (count + 1) (tail s)
+--         | head s == ')' = isBalanced' (count - 1) (tail s)
+--         | otherwise = isBalanced' count (tail s)
+
+-- Accumulating a value recursively (by destructuring a list)
+-- This is essentially the same as the foldl function which is available in Prelude
+reduce func carryValue lst =
+    if null lst then carryValue
+    else 
+        let intermediateValue = func carryValue (head lst)
+        in reduce func intermediateValue (tail lst)
+
+-- Reimplementation of isBalanced with the reduce function
+isBalanced str = 0 == reduce checkBalance 0 str
+    where
+    checkBalance count letter 
+        | letter == '(' = count + 1
+        | letter == ')' = count - 1
+        | otherwise = count
+
+-- Left-associative fold
+foldl func carryValue lst = 
+    if null lst
+    then carryValue
+    else foldl func (func carryValue (head lst)) (tail lst)
+
+-- Right-associative fold
+foldr func carryValue lst = 
+    if null lst
+    then carryValue
+    else func (head lst) $ foldr func carryValue (tail lst)

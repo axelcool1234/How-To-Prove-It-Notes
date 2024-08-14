f36099317d87f2936ffd88ea701080eb4a97b6a5
Axel Sorenson <AxelPSorenson@gmail.com>
2024-08-14 12:47:37 -0700

    Effective Haskell Chapter 1 exercises complete

--- effective-haskell/chapter1/Exercises.hs
+++ effective-haskell/chapter1/Exercises.hs
@@ -5,3 +5,12 @@ factorial = accum 1
         accum res num
             | num == 0 || num == 1 = res 
             | otherwise = accum (res * num) (num - 1)
+
+fibonacci = accum 1 0 
+    where
+        accum prev res counter
+            | counter == 0 = res   
+            | otherwise = accum res (prev + res) (counter - 1)
+
+myCurry func arg1 arg2 = func (arg1, arg2)
+myUncurry func tuple = func (fst tuple) (snd tuple)
